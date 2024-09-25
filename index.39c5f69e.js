8f37f5833732ea1fc26193ee00ea2b4658ade9a8
Kostya Vasilenko <kostyantin.vasilenko.03@gmail.com>
2024-09-25 21:31:19 +0300

    make build

---
+++ index.39c5f69e.js
@@ -0,0 +1,2 @@
+for(var e=function(e){t[e].addEventListener("click",function(r){r.preventDefault(),n.src=t[e].href})},t=document.querySelectorAll(".list-item__link"),n=document.querySelector("#largeImg"),r=0;r<t.length;r++)e(r);
+//# sourceMappingURL=index.39c5f69e.js.map
